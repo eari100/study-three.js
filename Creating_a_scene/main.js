efbdd5071a4f920745cc554cd860f8447e5844ad
Jaewook Choi <eari100@naver.com>
2023-09-11 23:05:16 +0900

    회전하는 큐브 주석 추가

--- Creating_a_scene/main.js
+++ Creating_a_scene/main.js
@@ -1,22 +1,27 @@
 import * as THREE from 'three'
 
 const scene = new THREE.Scene()
+// PerspectiveCamera
+// pov: 시야각, aspect: 화면 비율, near: 카메라 계산하는 최소 거리, far: 카메라가 계산하는 최대 거리
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize( window.innerWidth, window.innerHeight )
 document.body.appendChild( renderer.domElement )
 
+// BoxGeometry: 정육면체 객체
 const geometry = new THREE.BoxGeometry( 1, 1, 1 )
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } )
 const cube = new THREE.Mesh( geometry, material )
 scene.add( cube )
 
+// 카메라의 위치를 큐브 바깥쪽으로 이동, 큐브의 위치(0,0,0)
 camera.position.z = 5
 
 function animate() {
     requestAnimationFrame( animate )
 
+    // 큐브 회전
     cube.rotation.x += 0.01
     cube.rotation.y += 0.01
 
