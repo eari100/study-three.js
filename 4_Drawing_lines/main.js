34a9e534f67ec4b696ba2ebaf11e18d84e97494d
Jaewook Choi <eari100@naver.com>
2024-07-17 23:37:04 +0900

    update: 선분 2개 -> 삼각형으로 변경

--- 4_Drawing_lines/main.js
+++ 4_Drawing_lines/main.js
@@ -10,13 +10,15 @@ camera.lookAt( 0, 0, 0 )
 
 const scene = new THREE.Scene()
 
+// create a blue LineBasicMaterial
 const material = new THREE.LineBasicMaterial( { color: 0x0000ff } )
 
 const points = []
 points.push( new THREE.Vector3( -10, 0, 0 ) )
 points.push( new THREE.Vector3( 0, 10, 0 ) )
 points.push( new THREE.Vector3( 10, 0, 0 ) )
-points.push( new THREE.Vector3( 0, -10, 0 ) )
+points.push( new THREE.Vector3(-10, 0, 0) )
+
 
 const geometry = new THREE.BufferGeometry().setFromPoints( points )
 
